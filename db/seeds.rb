2cf7edc45fc8b241539833972f64fb441219c6a3
Leo Gonzalez <lgonz053@gmail.com>
2019-02-28 15:41:16 -0600

    adjusted seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,18 +2,12 @@ User.create!([
   {email: "jerseymike@gmail.com", gender: "male", name: "Mike", password: "password", password_confirmation: "password"},
   {email: "biganj@gmail.com", gender: "female", name: "Anjelica", password: "password", password_confirmation: "password"}
 ])
-<<<<<<< HEAD
 
-=======
->>>>>>> ce25407842b46473f21193d75a02f8ce0ccee2eb
 List.create!([
   {user_id: 1, name: "Antarctica"},
   {user_id: 1, name: "Ecuador"}
 ])
-<<<<<<< HEAD
 
-=======
->>>>>>> ce25407842b46473f21193d75a02f8ce0ccee2eb
 Item.create!([
   {name: "Jacket", list_id: 1},
   {name: "Tooth brush", list_id: 1},
@@ -22,8 +16,3 @@ Item.create!([
   {name: "Bug Spray", list_id: 2},
   {name: "Sunglasses", list_id: 2}
 ])
-<<<<<<< HEAD
-
-
-=======
->>>>>>> ce25407842b46473f21193d75a02f8ce0ccee2eb
